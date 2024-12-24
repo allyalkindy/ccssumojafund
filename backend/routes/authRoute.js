ff772ef12f8643caa8493ba97d4cb87ec3b722db
allyalkindy <allymohammedsaid126@gmail.com>
2024-12-24 12:01:35 +0300

    invalid error

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -16,14 +16,14 @@ router.post("/", async (req, res) => {
 
     const user = await userModel.findOne({ email: req.body.email });
     if (!user)
-      return res.status(401).send({ message: "Invalid Email or Password" });
+      return res.status(401).send({ message: "Invalid Email" });
 
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
     );
     if (!validPassword)
-      return res.status(401).send({ message: "Invalid Email or Password" });
+      return res.status(401).send({ message: "Invalid Password" });
 
     const token = user.generateAuthToken();
     res.status(200).send({ data: token, message: "logged in successfully" });
